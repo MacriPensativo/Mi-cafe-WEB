4c58392795ce74275abab3d3180a6fda4ac69062
MacriPensativo <81605185+MacriPensativo@users.noreply.github.com>
2025-04-21 22:35:49 -0300

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JC_PDWA2025.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
